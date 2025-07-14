167420ca4ccc8ae47b651a2f836ca88cc484bb3d
kam345678 <tk.kam23132@gmail.com>
2025-07-14 13:12:04 +0700

    tailwind install

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html", "./**/*.html"],   // สำคัญ! ให้ Tailwind scan HTML
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
